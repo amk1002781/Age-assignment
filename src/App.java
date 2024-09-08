426c50edd6b42b46bf803bcc1ec06f584117bbc7
amk1002781 <amk1002781@student.hamk.fi>
2024-09-08 19:43:33 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     int age = 45;
+
+
+     if(age == 18)
+     {
+       System.out.println(" yes,you can drive a car");
+     }
+     else
+     {
+        System.out.println("no, you cannot drive a car");
+     }
+     if(age>=10 && age<=120)
+     {
+        System.out.println("anniverasary party");
+     }
+     else
+     {
+        System.out.println("not any anniversary party");
+     }
+      if(age>=40 && age<=50)
+      {
+        System.out.println("yes,happy mid life");
+      }
+      else
+      {
+        System.out.println("no,happy mid life");
+      }
+    }
+}
